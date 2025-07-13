5e8cc6ab0b4d18f01691f2a3d6f4893b02209ef5
Ashutosh Kumar Pandey <ashutoshkumarp100@gmail.com>
2025-07-13 06:21:05 +0000

    variable concept

---
+++ basics/variable.js
@@ -0,0 +1,13 @@
+const accountId=1234  // const value cant change 
+let accountEmail="asd@gmail.com"
+var accountPassword="12333"
+accountCity="Sasaram"
+
+let accountState;
+
+
+accountEmail="baba@gmail.com"
+accountPassword="4321"
+accountCity="Varanasi"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
